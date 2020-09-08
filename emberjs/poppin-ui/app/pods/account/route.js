13a15b66506473c3a372af6f747d08643a55d7ff
Charles Jones <5iveblades@gmail.com>
2020-09-08 02:10:49 -0500

    eliminate `beforeModel` on account route

--- emberjs/poppin-ui/app/pods/account/route.js
+++ emberjs/poppin-ui/app/pods/account/route.js
@@ -1,9 +1,3 @@
 import Route from '@ember/routing/route';
-import { inject as service } from '@ember/service';
 
-export default class AccountRoute extends Route {
-	@service accountService;
-	beforeModel() {
-		return this.accountService.isAuthenticated();
-	}
-}
+export default class AccountRoute extends Route {}
