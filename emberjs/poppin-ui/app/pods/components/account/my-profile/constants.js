cdc1039d38d9aa231ccb7fea02cc0792151f5962
Charles Jones <5iveblades@gmail.com>
2020-10-27 21:24:54 -0500

    merge "UI" (3166a9a)

---
+++ emberjs/poppin-ui/app/pods/components/account/my-profile/constants.js
@@ -0,0 +1,11 @@
+export const states = {
+	IDLE: 'IDLE',
+	LOADING: 'LOADING',
+	EDIT_PROFILE: 'EDIT_PROFILE'
+};
+export const actions = {
+	START_EDIT: 'START_EDIT',
+	END_EDIT: 'END_EDIT',
+	REJECT_ACTION: 'REJECT_ACTION',
+	END_LOADING: 'END_LOADING'
+};
