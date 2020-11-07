67a716a63e8c156dc166a23d8a6f1d80b93b546b
Charles Jones <charles@wearepoppin.com>
2020-11-06 22:34:11 -0600

    updated docker settings

--- docker-build-local.bat
+++ docker-build-local.bat
@@ -4,9 +4,9 @@ cd emberjs/poppin-ui
 
 cd ../../
 CALL dotnet publish -c Debug -o ./dockerpub /p:EnvironmentName=Development
-CALL docker build -t poppin-app-debug .
+CALL docker build -t poppin-app-local .
 CALL aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin 750171440750.dkr.ecr.us-west-2.amazonaws.com
-CALL docker tag poppin-app-debug:latest 750171440750.dkr.ecr.us-west-2.amazonaws.com/poppin-app-debug:latest
-CALL docker push 750171440750.dkr.ecr.us-west-2.amazonaws.com/poppin-app-debug:latest
+CALL docker tag poppin-app-local:latest 750171440750.dkr.ecr.us-west-2.amazonaws.com/poppin-app-local:latest
+CALL docker push 750171440750.dkr.ecr.us-west-2.amazonaws.com/poppin-app-local:latest
 
 PAUSE
