22c38b885ad3a00fdb1ce1da37e647a575d1f5d2
Charles Jones <charles@wearepoppin.com>
2021-01-24 01:15:23 -0600

    > fix widths for mobile and tablet > added allow anonymous checkin from qr code > Geo Checkin worth .6, User Checkin worth 1.2 > anon checkin now Vendor value > refresh after checkin > loading spinner in modals > success/error alerts > hide pin link if not logged in

--- Models/Tracking/Checkin.cs
+++ Models/Tracking/Checkin.cs
@@ -7,37 +7,37 @@ using System.Threading.Tasks;
 
 namespace Poppin.Models.Tracking
 {
-				public static class ReliabilityScores
-				{
-								public static double Geo = 0.5;
-								public static double Vendor = 1.0;
-								public static double User = 1.5;
-				}
-				public class Checkin
-				{
-								public Checkin(string locationId, string userId, int visitLength, double reliabilityScore)
-								{
-												UserId = userId;
-												LocationId = locationId;
-												Timestamp = DateTime.Now;
-												Timeout = DateTime.Now.AddMinutes(visitLength);
-												ReliabilityScore = reliabilityScore;
-								}
+    public static class ReliabilityScores
+    {
+        public static double Geo = 0.6;
+        public static double Vendor = 1.0;
+        public static double User = 1.2;
+    }
+    public class Checkin
+    {
+        public Checkin(string locationId, string userId, int visitLength, double reliabilityScore)
+        {
+            UserId = userId;
+            LocationId = locationId;
+            Timestamp = DateTime.Now;
+            Timeout = DateTime.Now.AddMinutes(visitLength);
+            ReliabilityScore = reliabilityScore;
+        }
 
-								[BsonId]
-								[BsonRepresentation(BsonType.ObjectId)]
-								public string Id { get; set; }
-								public string UserId { get; set; }
-								public string LocationId { get; set; }
-								public DateTime Timestamp { get; set; }
-								public DateTime Timeout { get; set; }
-								public double ReliabilityScore { get; set; }
-								public bool IsValid
-								{
-												get
-												{
-																return DateTime.Now > Timeout;
-												}
-								}
-				}
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        public string UserId { get; set; }
+        public string LocationId { get; set; }
+        public DateTime Timestamp { get; set; }
+        public DateTime Timeout { get; set; }
+        public double ReliabilityScore { get; set; }
+        public bool IsValid
+        {
+            get
+            {
+                return DateTime.Now > Timeout;
+            }
+        }
+    }
 }
